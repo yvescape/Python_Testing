c422b641d6aa00ca45201e22a43d07b683278e77
unknown <yvesjuniorcape@gmail.com>
2024-02-05 22:04:45 +0000

    TEST008

---
+++ test/tests_unitaire/tests_reservation.py
@@ -0,0 +1,53 @@
+import unittest
+from flask import Flask, request
+from server import app  # Assurez-vous d'importer correctement votre application Flask et ses dépendances
+
+class FlaskTestCase(unittest.TestCase):
+
+    def setUp(self):
+        app.config['TESTING'] = True
+        self.app = app.test_client()
+
+    def test_purchase_places_valid(self):
+        # Cas où le nombre de places est valide
+        data = {
+            'competition': "Spring Festival",
+            'club': "Simply Lift",
+            'places': '5'
+        }
+        response = self.app.post('/purchasePlaces', data=data)
+        self.assertEqual(response.status_code, 200)  # Assurez-vous que la redirection fonctionne correctement
+
+    def test_purchase_places_invalid(self):
+        # Cas où le nombre de places est valide
+        data = {
+            'competition': "Spring Festival",
+            'club': "Simply Lift",
+            'places': '0'
+        }
+        response = self.app.post('/purchasePlaces', data=data)
+        self.assertEqual(response.status_code, 302)  # Assurez-vous que la redirection fonctionne correctement
+
+    def test_purchase_places_sup(self):
+        # Cas où le nombre de places est valide
+        data = {
+            'competition': "Spring Festival",
+            'club': "Simply Lift",
+            'places': '50'
+        }
+        response = self.app.post('/purchasePlaces', data=data)
+        self.assertEqual(response.status_code, 302)  # Assurez-vous que la redirection fonctionne correctement
+
+    def test_purchase_places_sup_club(self):
+        # Cas où le nombre de places est valide
+        data = {
+            'competition': "Spring Festival",
+            'club': "Iron Temple",
+            'places': '6'
+        }
+        response = self.app.post('/purchasePlaces', data=data)
+        self.assertEqual(response.status_code, 302)  # Assurez-vous que la redirection fonctionne correctement
+        # Ajoutez d'autres assertions si nécessaire
+
+if __name__ == '__main__':
+    unittest.main()
